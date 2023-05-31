e67df0122f13ec958bb670450b7eb1d132887dbf
Gabriel Jean <imjeangabriel@gmail.com>
2023-05-31 12:31:18 +0300

    Firebase configuration

---
+++ utils/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
+};
+
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
